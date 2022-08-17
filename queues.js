1ae2f269f49c75cc5c35f7eba8460e65da6f06e1
tonyishangu <tonyishangu@gmail.com>
2022-08-17 08:27:19 +0300

    create enqueue method

--- queues.js
+++ queues.js
@@ -12,4 +12,27 @@ class Queue {
         this.last = null
         this.size = 0
     }
+
+    // enqueue
+    // adding to a queue
+        // fuction accepts value
+        // create a new node
+        // if there are no nodes set first and last to be new node
+        // else set the next property of the current last to be of new node 
+        // set last property to be new node
+        // increment size 
+        // return queue
+
+    enqueue = (val) => {
+        const newNode = new Node(val)
+        if(this.size === 0){
+            this.first = newNode
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        this.size ++
+        return this
+    }
 }
